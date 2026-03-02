0d66355f5817d87d69bad12a567bda0ad14c162c
Jonathan Bogerd <jonathanbogerd@gmail.com>
2026-03-02 20:18:58 +0000

    Move to libstdc++

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 # Set the build mode (Release or Debug, for example)
 BuildMode=${1:-Release} # Default to Release if no argument is provided
 
-export CXXFLAGS=-stdlib=libc++
+export CXXFLAGS=-stdlib=libstdc++
 # Create the build directory
 mkdir -p ./build/${BuildMode}
 
